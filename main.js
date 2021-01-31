618898f5b9e542e65d9b2042ef2120b208b86484
YanaWhitte <shiro.kirio@gmail.com>
2021-01-31 22:26:46 +0200

    React-based implementation started

--- main.js
+++ main.js
@@ -1,48 +1,62 @@
-const femaleBtn = document.querySelector(".female-btn");
-femaleBtn.addEventListener("click", () => {
-  fetch("https://cat-names-api.herokuapp.com/random/female")
-    .then((response) => response.json())
-    .then((json) => {
-      document.getElementById("result").innerHTML = json.name;
-    });
-});
+const root = document.querySelector("#root");
+const h = React.createElement;
 
-const maleBtn = document.querySelector(".male-btn");
-maleBtn.addEventListener("click", () => {
-  fetch("https://cat-names-api.herokuapp.com/random/male")
-    .then((response) => response.json())
-    .then((json) => {
-      document.getElementById("result").innerHTML = json.name;
-    });
-});
-
-function setName(name) {
-  document.querySelector("#result").innerHTML = name;
-  search.value = "";
-  dropList.classList.remove("active");
+function Title() {
+  return h("h1", { className: "title" }, "Cat Name Generator");
 }
 
-const search = document.querySelector("input");
-const dropList = document.querySelector(".search__droplist");
-search.addEventListener("keyup", (event) => {
-  const searchValue = event.target.value;
+function Content() {
+  const [petName, setPetName] = React.useState("random name");
 
-  fetch(
-    `https://cat-names-api.herokuapp.com/search/unisex?filter=${searchValue}`
-  )
-    .then((response) => response.json())
-    .then((names) => {
-      if (searchValue !== "" && names.length > 0) {
-        dropList.classList.add("active");
-      } else {
-        dropList.classList.remove("active");
-      }
+  const makeFetchAction = (gender) => {
+    return () => {
+      fetchData(`random/${gender}`).then((json) => {
+        setPetName(json.name);
+      });
+    };
+  };
 
-      dropList.innerHTML = "";
-      for (let name of names) {
-        dropList.innerHTML += `
-        <p class="search__result" onclick="setName('${name}')">${name}</p>
-        `;
-      }
-    });
-});
+  return h("div", { className: "content" }, [
+    h(Button, {
+      key: "male",
+      name: "male name",
+      onClick: makeFetchAction("male"),
+    }),
+    h("p", { className: "result", key: "p" }, petName),
+    h(Button, {
+      key: "female",
+      name: "female name",
+      onClick: makeFetchAction("female"),
+    }),
+  ]);
+}
+
+function Button(props) {
+  return h("div", { className: "btn", onClick: props.onClick }, [
+    h("img", { className: "paw", src: "img/paw.svg", key: "img" }),
+    h("p", { className: "name", key: "p" }, props.name),
+  ]);
+}
+
+function Search() {
+  return h("div", { className: "search" }, [
+    h("input", { type: "search", placeholder: "search...", key: "input" }),
+    h("img", { className: "search__icon", src: "img/search.svg", key: "img" }),
+    h("div", { className: "search__droplist", key: "div" }),
+  ]);
+}
+
+ReactDOM.render(
+  h("section", { className: "container" }, [
+    h(Title, { key: "Title" }),
+    h(Content, { key: "Content" }),
+    h(Search, { key: "Search" }),
+  ]),
+  root
+);
+
+function fetchData(path) {
+  return fetch(`https://cat-names-api.herokuapp.com/${path}`).then((response) =>
+    response.json()
+  );
+}
