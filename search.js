d31836f52ad13f06af4cbe3044db24e5ff731d6b
YanaWhitte <shiro.kirio@gmail.com>
2021-02-01 21:40:43 +0200

    Functional seatch input

--- search.js
+++ search.js
@@ -1,15 +1,58 @@
 function Search(props) {
   const petName = props.petName;
   const setPetName = props.setPetName;
+  const [dropListVisible, setDropListVisible] = React.useState(false);
+  const [names, setNames] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
 
   return h("div", { className: "search" }, [
     h("input", {
       type: "search",
       placeholder: "search...",
       key: "input",
-      onKeyUp: () => {},
+      value: searchValue,
+      onChange: (event) => setSearchValue(event.target.value),
+      onKeyUp: (event) => {
+        const searchValue = event.target.value;
+
+        fetch(
+          `https://cat-names-api.herokuapp.com/search/unisex?filter=${searchValue}`
+        )
+          .then((response) => response.json())
+          .then((newNames) => {
+            if (searchValue !== "" && newNames.length > 0) {
+              setDropListVisible(true);
+            } else {
+              setDropListVisible(false);
+            }
+
+            setNames(newNames);
+          });
+      },
     }),
     h("img", { className: "search__icon", src: "img/search.svg", key: "img" }),
-    h("div", { className: "search__droplist", key: "div" }),
+    h(
+      "div",
+      {
+        className: dropListVisible
+          ? "active search__droplist"
+          : "search__droplist",
+        key: "div",
+      },
+      names.map((name) =>
+        h(
+          "p",
+          {
+            className: "search__result",
+            onClick: () => {
+              setPetName(name);
+              setDropListVisible(false);
+              setSearchValue("");
+            },
+          },
+          name
+        )
+      )
+    ),
   ]);
 }
